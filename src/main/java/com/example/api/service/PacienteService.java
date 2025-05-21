478272df5e4b7e97f5546c88516c20512d9f4ac1
Alex Coelho <alexcastelo437@mail.com>
2025-05-21 13:29:38 -0300

    Criado service de paciente

---
+++ src/main/java/com/example/api/service/PacienteService.java
@@ -0,0 +1,83 @@
+package com.example.api.service;
+
+import com.example.api.dto.atualizar.AtualizarPacienteDto;
+import com.example.api.dto.criar.CriarPacienteDto;
+import com.example.api.exception.CpfException;
+import com.example.api.exception.ResourceNotFound;
+import com.example.api.model.Paciente;
+import com.example.api.repository.PacienteRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+
+import java.util.UUID;
+
+@Service
+public class PacienteService {
+
+    final PacienteRepository pacienteRepository;
+
+    public PacienteService(PacienteRepository pacienteRepository) {
+        this.pacienteRepository = pacienteRepository;
+    }
+
+    public Paciente criarPaciente(CriarPacienteDto pacienteDto) {
+        Paciente paciente = new Paciente();
+        if (pacienteRepository.existsByCpf(pacienteDto.cpf())){
+            throw new CpfException("Cpf já registrado no sistema");
+        }
+
+        BeanUtils.copyProperties(pacienteDto, paciente);
+
+        return pacienteRepository.save(paciente);
+    }
+
+    public Page<Paciente> listarPaciente(Pageable pageable) {
+        return pacienteRepository.findAll(pageable);
+    }
+
+
+    public Paciente listarUmPaciente(UUID id) {
+        return pacienteRepository.findById(id).orElseThrow(() -> new ResourceNotFound("Paciente não localizado"));
+    }
+
+
+
+    public void atualizarPaciente(UUID id, AtualizarPacienteDto pacienteDto) {
+        var paciente = pacienteRepository.findById(id);
+        if (paciente.isPresent()) {
+            Paciente pacienteAtualizar = paciente.get();
+
+            if (pacienteDto.nome() != null){
+                pacienteAtualizar.setNome(pacienteDto.nome());
+            }
+
+            if (pacienteDto.idade() != null){
+                pacienteAtualizar.setIdade(pacienteDto.idade());
+            }
+
+            if (pacienteDto.contato() != null){
+                pacienteAtualizar.setContato(pacienteDto.contato());
+            }
+
+            if (pacienteDto.endereco() != null){
+                pacienteAtualizar.setEndereco(pacienteDto.endereco());
+            }
+
+            pacienteRepository.save(pacienteAtualizar);
+        } else {
+            throw new ResourceNotFound("Paciente não localizado");
+        }
+
+    }
+
+    public void deletarPaciente(UUID id) {
+        var paciente =pacienteRepository.findById(id);
+        if (paciente.isEmpty()) {
+            throw new ResourceNotFound("Paciente não localizado");
+        }
+        pacienteRepository.delete(paciente.get());
+    }
+}
