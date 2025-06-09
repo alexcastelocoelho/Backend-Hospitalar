7375b8abe8776f1007f8fcc9e80e93d8007a9ebb
Alex Coelho <alexcastelo437@mail.com>
2025-06-09 14:52:28 -0300

    Criado repository de usuario

---
+++ src/main/java/com/example/api/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.example.api.repository;
+
+import com.example.api.model.usuario.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+
+    Optional<Usuario> findByLogin(String login);
+}
+
