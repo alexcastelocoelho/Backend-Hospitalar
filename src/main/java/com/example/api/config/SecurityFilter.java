a10d34a921da692766f55ad894a2afeb50c21c99
Alex Coelho <alexcastelo437@mail.com>
2025-06-09 14:56:47 -0300

    Definido service para criar e validar token jwt e filtro de seguranca

---
+++ src/main/java/com/example/api/config/SecurityFilter.java
@@ -0,0 +1,50 @@
+package com.example.api.config;
+
+import com.example.api.repository.UsuarioRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    final TokenService tokenService;
+    final UsuarioRepository usuarioRepository;
+
+    public SecurityFilter(TokenService tokenService, UsuarioRepository usuarioRepository) {
+        this.tokenService = tokenService;
+        this.usuarioRepository = usuarioRepository;
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = recoverToken(request);
+        if(token != null){
+            var login = tokenService.validateToken(token);
+            var usuario = usuarioRepository.findByLogin(login);
+            if (usuario.isPresent()) {
+                var usuarionovo = usuario.get();
+                var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.get().getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
+
+
+}
