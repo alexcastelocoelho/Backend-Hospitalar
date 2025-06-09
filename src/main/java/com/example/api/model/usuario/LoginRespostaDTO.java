b08958b11143437964177f0e029ebfb34f0b6a33
Alex Coelho <alexcastelo437@mail.com>
2025-06-09 14:59:04 -0300

    Definido dto para fazer login e resposta personalizada

---
+++ src/main/java/com/example/api/model/usuario/LoginRespostaDTO.java
@@ -0,0 +1,4 @@
+package com.example.api.model.usuario;
+
+public record LoginRespostaDTO(String token) {
+}
---
+++ src/main/java/com/example/api/model/usuario/RealizarLoginDto.java
@@ -0,0 +1,18 @@
+package com.example.api.model.usuario;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record RealizarLoginDto(
+
+        @Email(message = "Informe o seu e-mail de acesso")
+        @NotBlank(message = "Obrigario informar o e-mail.")
+        String login,
+
+        @NotBlank(message = "Informe sua senha de acesso")
+        @Size(min = 6, message = "Sua senha deve ter pelo menos 6 caracteres.")
+        String password
+
+) {
+}
